06265ef8842d2e9c0446b018f8a1fe2f9b270511
William Whinn <w.willis.1986@gmail.com>
2021-06-10 15:43:24 +0100

    Repair quantile NaN errors

--- src/gillespie_model.jl
+++ src/gillespie_model.jl
@@ -1,4 +1,3 @@
-using Plots: length
 #!/usr/bin/env julia
 
 ###########
@@ -27,14 +26,13 @@ using Plots: length
 # Notes #
 #########
 
-# - Code written to detect the time and concentration arrays will not
-#   function correctly for Julia versions older than 1.6.x.
+# - Requires Julia >= v1.6.
 
 ##############
 # References #
 ##############
 
-# <https://nextjournal.com/bebi5009/gillespie-julia>
+# - <https://nextjournal.com/bebi5009/gillespie-julia>
 
 ###########
 # Imports #
@@ -63,14 +61,17 @@ Random.seed!(41269)
 # Variables #
 #############
 
-# Initial concentrations of species 'A' and 'B'.
-u0 = [150, 50]
+# Initial concentrations of wild-type and mutant mtDNA.
+u0 = [200, 50]
 
 # Time at which the simulation will stop.
-tend = 10.0  # tend > 27 will crash.
+tend = 500.0
 
 # Kinetic rates of reactions.
-parameters = (r=1.0, m=0.0, d=1.0)
+parameters = (r=0.01, m=0.0, d=0.01)
+
+# Number of simulation repeats.
+loops = 1000
 
 #############
 # Functions #
@@ -93,10 +94,10 @@ function ssa(model, u0, tend, p, choose_stoich, tstart=zero(tend); delta=0.1)
     tindex = 2  # Initial step is already defined.
 
     while t < tend && u[1] + u[2] > 0
-        # If time > next sample, do this. Update sample to be +1 week.
         if t >= times[tindex]
             us = [us u]
-            # push!(ts, times(tindex))
+            # push!(ts, times(tindex))  # New, Non-working.
+            push!(ts, t)                # Old, working.
             tindex = tindex + 1
         end
 
@@ -118,12 +119,13 @@ end
 `model(u, p, t)`
 
 Propensity model for this reaction.
-Reaction of `A <-> B` with rate constants `k1` & `k2`.
+Reaction of `Wild-type <-> Mutant` with rate constants `k1` & `k2`.
 """
 function model(u, p, t)
-
+    # Define copy number.
     cn = u[1] + u[2]
 
+    # Prevent early extinction.
     if cn < 50
         r1 = p.r * u[1]
         r4 = p.r * u[2]
@@ -132,10 +134,10 @@ function model(u, p, t)
         r4 = 0
     end
 
-    return [r1,  # Reaction 1: Replicate wild-type mtDNA.
+    return [r1,          # Reaction 1: Replicate wild-type mtDNA.
             p.d * u[1],  # Reaction 2: Degrade wild-type mtDNA.
             p.m * u[1],  # Reaction 3: Mutate wild-type mtDNA.
-            r4,  # Reaction 4: Replicate mutant mtDNA.
+            r4,          # Reaction 4: Replicate mutant mtDNA.
             p.d * u[2]]  # Reaction 5: Degrade mutant mtDNA.
 end
 
@@ -222,14 +224,13 @@ in the `gillespie_model.jl` file.
 ```julia-repl
 julia> loop_simulation(10)
 ```
-
 """
 function loop_simulation(n::Int64)
     # Create new arrays for holding all results.
     time_states = []
     concentration_states = []
 
-    println("Simulation will be performed $(n) time(s).")
+    print("\nSimulation is repeating $(n) time(s)... ")
 
     # Start at loop number 1 in intervals of 1 until 'n'.
     for i in 1:1:n
@@ -242,34 +243,46 @@ function loop_simulation(n::Int64)
         push!(concentration_states, sol.u)
     end
 
-    return(t = time_states, c = concentration_states)
+    println("Done")
+
+    return(t = time_states, u = concentration_states)
 end
 
+# Functions > NaN Handling
+
+nanmean(x) = mean(filter(!isnan, x))
+nanmean(x, y) = mapslices(nanmean, x, dims=y)
+
+nanmedian(x) = median(filter(!isnan, x))
+nanmedian(x, y) = mapslices(nanmedian, x, dims=y)
+
+nanquantile(x, y) = quantile(filter(!isnan,x), y)
 
 #############
 # Kickstart #
 #############
 
-# Run simulation 'n' time(s) and assign returns to 'results'.
-results = loop_simulation(1000)
+# Run simulation 'loops' time(s) and assign output to 'results'.
+results = loop_simulation(loops)
 
 # Calculate total number of elements.
-num_simulations = size(results.c)[1]
-num_times = size(results.c[1])[1]
-num_species = size(results.c[1])[2]
+num_simulations = size(results.u)[1]
+num_times = size(results.u[1])[1]
+num_species = size(results.u[1])[2]
 
 # Create a list of times.
 times = results.t[1]
 
 # Preallocate array for states.
-# molecules = Array{Float64}(undef, num_simulations, num_times, num_species)
 molecules = fill(NaN, num_simulations, num_times, num_species)
 
-for i in 1:num_simulations  # Error if tend > 27.
+# Populate 'molecules' array.
+# NOTE Empty spaces will contain NaN.
+for i in 1:num_simulations
     for j in 1:num_times
         for k in 1:num_species
-            if size(results.c[i])[1] >= j
-                molecules[i, j, k] = results.c[i][j, k]  # Error if tend > 27.
+            if size(results.u[i])[1] >= j
+                molecules[i, j, k] = results.u[i][j, k]
             end
         end
     end
@@ -278,56 +291,66 @@ end
 # Calculate total number of molecules.
 total = sum(molecules, dims=3)
 
-# Calculate percentage of mutant mtDNA.
+# Calculate percentage of wild-type and mutant mtDNA.
+wild_load = molecules[:, :, 1] ./total
 mutation_load = molecules[:, :, 2] ./ total
 
-nanmean(x) = mean(filter(!isnan,x))
-nanmean(x,y) = mapslices(nanmean,x,dims=y)
-
-# mean_mutant = mean(mutation_load, dims=1)
+# Calculate metrics for plotting.
+mean_wild = nanmean(wild_load, 1)
 mean_mutant = nanmean(mutation_load, 1)
-median_mutant = median(mutation_load, dims=1)
+median_wild = nanmedian(wild_load, 1)
+median_mutant = nanmedian(mutation_load, 1)
 
-# EXPERIMENTAL Flatten objects to simple arrays.
+# Flatten objects to simple arrays.
+mean_wild = collect(Iterators.flatten(mean_wild))
 mean_mutant = collect(Iterators.flatten(mean_mutant))
+median_wild = collect(Iterators.flatten(median_wild))
 median_mutant = collect(Iterators.flatten(median_mutant))
 
-# Calculate percentage of wild-type mtDNA.
-wild_load = molecules[:, :, 1] ./total
 
-mean_wild = nanmean(wild_load, 1)
-median_wild = median(wild_load, dims=1)
+# DEFINE QUANTILES
 
-# EXPERIMENTAL Flatten objects to simple arrays.
-mean_wild = collect(Iterators.flatten(mean_wild))
-median_wild = collect(Iterators.flatten(median_wild))
-
-upper_quantile = Array{Float64}(undef, num_times)
-lower_quantile = Array{Float64}(undef, num_times)
+upper_quantile = fill(NaN, num_times)
+lower_quantile = fill(NaN, num_times)
 
 for j in 1:num_times
     mutation_loads = mutation_load[:, j]
-    upper_quantile[j] = quantile(mutation_loads, 0.975)  # FIXME
-    lower_quantile[j] = quantile(mutation_loads, 0.025)  # FIXME
+    upper_quantile[j] = nanquantile(mutation_loads, 0.975)
+    lower_quantile[j] = nanquantile(mutation_loads, 0.025)
 end
 
-# Create plot axis elements.
+
+# DEFINE PLOT
+
+# Define plot axis elements.
 x = times
 y = [mean_wild, mean_mutant]
 # y = [median_wild, median_mutant]
 
-# Create plot of median values.
+print("Writing plot to '$(pwd())/ssa_plot.png'... ")
 fig = plot(
     x,  # Temporal States
-    y,  # Molecule Concentration
+    y,  # Molecule Concentration [Wild-type, Mutant]
     xlabel="Time",
     ylabel="Number of Molecules (%)",
-    # xlims=(0, 10), # Between 0 and 10 for time states.
-    ylims=(0, 1),  # Between 0 and 1 for percentage.
+    xlims=(1, length(mean_mutant)),
+    lims=(0, 1),
     title="mtDNA Population Dynamics (SSA Model)",
     label=["Wild-type" "Mutant"],
-    dpi=300)
+    dpi=300
+)
 
-savefig(fig, "plot_2.png")
+# Save plot in current working directory.
+savefig(fig, "$(pwd())/ssa_plot.png")
+println("Done")
+
+
+# TEXT REPORT
+
+println("\nRESULTS\n=======\n")
+println("Simulation was looped $(loops) time(s).")
+println("Populations extinct at: $(length(mean_mutant))/$(trunc(Int, tend)) Cycle(s).")
+println("Upper quantile (Head): $(upper_quantile[1:5])")
+println("Lower quantile (Head): $(lower_quantile[1:5])")
 
 # End of File.
