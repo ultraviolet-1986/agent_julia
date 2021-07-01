b42a295a426c9ddf8e828fd41552564d9202dac7
William Whinn <w.willis.1986@gmail.com>
2021-07-01 13:56:58 +0100

    Include reference

--- src/Agent Julia Model/integrated_cpu_support.sh
+++ src/Agent Julia Model/integrated_cpu_support.sh
@@ -22,6 +22,12 @@
 # You should have received a copy of the GNU General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 
+##############
+# References #
+##############
+
+# - <https://makie.juliaplots.org/stable/backends_and_output.html>
+
 #########
 # Notes #
 #########
