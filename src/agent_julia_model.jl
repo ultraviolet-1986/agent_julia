5c15ecb24d0c2ecdf3ed9d891a43759333f7db51
William Whinn <w.willis.1986@gmail.com>
2021-10-21 18:15:04 +0100

    Optimise Agent code

--- src/agent_julia_model.jl
+++ src/agent_julia_model.jl
@@ -41,9 +41,6 @@ using DrWatson: @dict
 using InteractiveDynamics
 using Random
 
-using IterTools
-using Statistics
-
 #################
 # Prerequisites #
 #################
@@ -57,24 +54,26 @@ Random.seed!(random_seed)
 
 # TEMPORAL UNITS
 
-day = Float64(24.0 * 3600.0)  # Seconds per day.
-week = Float64(day * 7.0)         # Seconds per week.
-year = Float64(day * 365.0)       # Seconds per year.
-month = Float64(year / 12.0)      # Seconds per month.
-hour = Float64(day / 24.0)        # Seconds per hour.
+day = Float64(24.0 * 3600.0)
+week = Float64(day * 7.0)
+year = Float64(day * 365.0)
+month = Float64(year / 12.0)
+hour = Float64(day / 24.0)
 
-# Reaction rate
+# REACTION RATES
+
 λ = 260.0
 reaction_rate = log(2) / λ
-reaction_rate = reaction_rate * 365 / 12
+reaction_rate = reaction_rate * 365.0 / 12.0
 
-# TODO Define replication, degradation, and mutation rates seperately.
+replication_rate = reaction_rate
+degradation_rate = reaction_rate
+# mutation_rate = reaction_rate
+
+# SIMULATION STEPPING
 
 δ = month
-
-# NOTE Stepping functions require type 'Int'.
-
-tend = Int(round(year * 80.0 / δ))  # 1 step is equal to 1 month.
+tend = Int(round(year * 80.0 / δ))
 
 # COLOURS > TEXT OUTPUT
 
@@ -128,13 +127,13 @@ end
 # Functions #
 #############
 
-
 """
 `mutation_initiation(;
     interaction_radius = 0.012,
     dt = δ,
-    reaction_rate = hour,
-    N = agent_max,
+    replication_rate = replication_rate,
+    degradation_rate = degradation_rate,
+    N = agent_total,
     initial_mutated = initial_mutants,
     seed = random_seed,
 )`
@@ -144,14 +143,18 @@ Create and populate the Agent Julia model with specified agents.
 function mutation_initiation(;
     interaction_radius = 0.012,
     dt = δ,
-    reaction_rate = reaction_rate,
+    replication_rate = replication_rate,
+    degradation_rate = degradation_rate,
+    # mutation_rate = mutation_rate,
     N = agent_total,
     initial_mutated = initial_mutants,
     seed = random_seed,
 )
 
     properties = @dict(
-        reaction_rate,
+        replication_rate,
+        degradation_rate,
+        # mutation_rate
         interaction_radius,
         dt,
     )
@@ -165,7 +168,7 @@ function mutation_initiation(;
         rng=MersenneTwister(seed),
     )
 
-    for ind in 1:N
+    for ind in 1:1:N
         pos = (rand(Uniform(0.0, graph_width)), rand(Uniform(0.0, graph_height)))
         status = ind <= N - initial_mutated ? :W : :M
         mass = 1.0
@@ -211,7 +214,7 @@ function random_action!(agent, model)
     copy_number = length(model.agents)
 
     # Replicate mtDNA
-    if roll <= reaction_rate
+    if roll <= replication_rate
         if copy_number <= agent_max
             pos = agent.pos
             status = agent.status
@@ -222,7 +225,7 @@ function random_action!(agent, model)
         end
 
     # Degrade mtDNA
-    elseif (roll > reaction_rate) && (roll <= (reaction_rate * 2))  # Change var names
+    elseif (roll > replication_rate) && (roll <= (replication_rate + degradation_rate))
         if copy_number >= agent_min
             kill_agent!(agent, model)
         end
@@ -238,74 +241,30 @@ end
 
 
 """
-`perform_simulation()`
+`perform_simulation(n=1::Int64)`
 
 Run the simulation through the `loop_simulation(n)` function `n`
 time(s).
 """
-function perform_simulation()
-    print("Performing simulation $(yellow)$(loops)$(reset) time(s). Please wait... ")
-    data = loop_simulation(loops)
-    println("$(green)Done$(reset)")
-
-    return data
-end
-
-
-"""
-`loop_simulation(n=1::Int64)`
-
-Perform the Agent Julia simulation `n` number of times.
-"""
-function loop_simulation(n=1::Int64)
-    temp_results = []
+function perform_simulation(n=1::Int64)
+    complete_results = []
 
     for i in 1:1:n
-        # Purge and redefine model for new simulation.
+        adata = nothing
         agent_julia_model = nothing
+
         agent_julia_model = mutation_initiation()
 
         wild(x) = count(i == :W for i in x)
         mutant(x) = count(i == :M for i in x)
 
-        # Purge and redefine 'adata' for new simulation.
-        adata = nothing
         adata = [(:status, wild), (:status, mutant)]
 
         data, _ = run!(agent_julia_model, agent_step!, model_step!, tend; adata)
-        push!(temp_results, data)
+        push!(complete_results, data)
     end
 
-    # Create final composite results.
-    steps = eachcol(temp_results[1])[1]
-    wild_counts = []
-    mutant_counts = []
-    total_counts = []
-
-    for j in 1:1:length(temp_results)
-        wild_count = eachcol(temp_results[j])[2]
-        mutant_count = eachcol(temp_results[j])[3]
-        total_cn = eachcol(temp_results[j])[2] + eachcol(temp_results[j])[3]
-
-        push!(wild_counts, wild_count)
-        push!(mutant_counts, mutant_count)
-        push!(total_counts, total_cn)
-    end
-
-    wild_mean   = mean(wild_counts)
-    mutant_mean = mean(mutant_counts)
-    total_mean  = mean(total_counts)
-
-    composite_results = DataFrame(
-        steps = steps,
-        wild_mean = wild_mean,
-        mutant_mean = mutant_mean,
-        total_mean = total_mean
-    )
-
-    complete_results = temp_results
-
-    return composite_results, temp_results
+    return complete_results
 end
 
 
@@ -316,33 +275,38 @@ Run a single simulation and output the results to video.
 """
 function output_simulation_to_video()
     video_path = "$(Base.source_dir())/videos"
-    mp4_path = "$(video_path)/agent_julia_simulation.mp4"
+    mp4_name = "agent_julia_simulation.mp4"
+    mp4_path = "$(video_path)/$(mp4_name)"
 
     rm(video_path; force=true, recursive=true)
     mkpath(video_path)
+
+    steps = Int(length(results.wild_status[1]) - 1)
 
     print("\nDefining simulation colour palette... ")
     model_colours(a) = a.status == :W ? green_hex : red_hex
     println("$(green)Done$(reset)")
 
-    print("Rendering simulation output as $(yellow)$(mp4_path)$(reset)... ")
+    print("Rendering simulation output as $(yellow)$(mp4_name)$(reset)... ")
     try
+        agent_julia_model = nothing
+        agent_julia_model = mutation_initiation()
         abm_video(
             "$(mp4_path)",
             agent_julia_model,
             agent_step!,
             model_step!;
             title = "Patient with mutant inheritance",
-            frames = tend,
+            frames = steps,
             ac = model_colours,
             as = 10,
-            spf = Int(round(δ)),
+            spf = Int(1),
             framerate = 60,
         )
-        println("$(green)Done\n$(reset)")
+        println("$(green)Done$(reset)")
     catch
         println("$(red)Error$(reset)")
-        println("Please run the $(yellow)integrated_gpu_support.sh$(reset) script.\n")
+        println("Please run the $(yellow)integrated_gpu_support.sh$(reset) script.")
     end
 end
 
@@ -357,34 +321,40 @@ println("======================\n")
 println("‣ Maximum possible agents....: $(green)$(agent_max)$(reset)")
 println("‣ Total agents...............: $(green)$(agent_total)$(reset)")
 println("‣ Wild-type agents...........: $(green)$(initial_wild)$(reset)")
-println("‣ Mutant elements............: $(green)$(initial_mutants)$(reset)")
+println("‣ Mutant agents..............: $(green)$(initial_mutants)$(reset)")
 println("‣ Simulation loops...........: $(green)$(loops)$(reset)\n")
 
-print("Creating mtDNA population dynamics model... ")
-agent_julia_model = nothing
-agent_julia_model = mutation_initiation()
+print("Performing simulation $(yellow)$(loops)$(reset) time(s)... ")
+results = perform_simulation(loops)
 println("$(green)Done$(reset)")
 
-# Execute Agent Julia simulation.
-data, results = perform_simulation()
+
+# KICKSTART > CALCULATE MEAN RESULTS
+
+results = DataFrame(results)
+
+mean_results = DataFrame(
+    wild_mean = mean(results.wild_status),
+    mutant_mean = mean(results.mutant_status),
+    total_mean = mean(results.wild_status) + mean(results.mutant_status)
+)
 
 
-# KICKSTART > DATA OUTPUT
+# KICKSTART > WRITE CSV PER SIMULATION
 
 rm(data_path; force=true, recursive=true)
 mkpath(data_path)
 
 print("Writing all data to $(yellow)$(loops)$(reset) CSV file(s)... ")
-results = DataFrame(results)
-for i in 1:1:loops
-    wild = results[i, 2]
-    mutant = results[i, 3]
+for k in 1:1:loops
+    wild = results[k, 2]
+    mutant = results[k, 3]
 
     temp = DataFrame(wild_count=wild, mutant_count=mutant)
 
     padding = Int(length(string(length(results.step))))
 
-    fname = "$(lpad(i, padding, '0')).csv"
+    fname = "$(lpad(k, padding, '0')).csv"
     CSV.write("$(data_path)/$(fname)", temp)
 end
 println("$(green)Done$(reset)")
@@ -392,7 +362,7 @@ println("$(green)Done$(reset)")
 
 # KICKSTART > PROMPT ADDITIONAL STEPS
 
-println("""\nOPTIONAL: Use $(yellow)output_simulation_to_video()$(reset) function.""")
-println("""Plots may be created using the R script $(yellow)create_plots.R$(reset).\n""")
+println("\nOPTIONAL: Use $(yellow)output_simulation_to_video()$(reset) function.")
+println("Plots may be created using the R script $(yellow)create_plots.R$(reset).\n")
 
 # End of File.
---
+++ timing.md
@@ -0,0 +1,19 @@
+# Model Times
+
+These timings were gathered from executing the `time` command in BASH.
+
+## Agent Julia
+
+```text
+real    4m42.181s
+user    4m22.666s
+sys     0m18.886s
+```
+
+## Gillespie SSA
+
+```text
+real    0m25.026s
+user    0m24.813s
+sys     0m0.514s
+```
